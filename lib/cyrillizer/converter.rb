c43d8d08c9913b3a36c25d94e3b64745bc4a8b0b
vasil <vasil.taneski@gmail.com>
2009-10-25 06:55:11 -0700

    indent fix

--- lib/cyrillizer/converter.rb
+++ lib/cyrillizer/converter.rb
@@ -1,16 +1,15 @@
 # coding: utf-8
 module Cyrillizer
-	module Converter
-
+  module Converter
     Latin    = "AaBbVvGgDdEeZzIiJjKkLlMmNnOoPpRrSsTtUuFfHhCc"
     Cyrillic = "АаБбВвГгДдЕеЗзИиЈјКкЛлМмНнОоПпРрСсТтУуФфХхЦц"
 
     Latin_a    = %w(DZH Dzh dzh GJ Gj gj ZH Zh zh DZ Dz dz LJ Lj lj NJ Nj nj KJ Kj kj CH Ch ch SH Sh sh)
     Cyrillic_a = %w(Џ   Џ   џ   Ѓ  Ѓ  ѓ  Ж  Ж  ж  Ѕ  Ѕ  ѕ  Љ  Љ  љ  Њ  Њ  њ  Ќ  Ќ  ќ  Ч  Ч  ч  Ш  Ш  ш )
 
-	  def to_cyr
+    def to_cyr
       self.tr_group(Latin_a, Cyrillic_a).tr(Latin, Cyrillic)
-	  end
+    end
 
     protected
     def tr_group(from, to)
@@ -20,7 +19,7 @@ module Cyrillizer
       }
       return text
     end
-	end
+  end
 end
 
 # Add to_cyr method to all Strings
